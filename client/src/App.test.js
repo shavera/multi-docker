58fc5a6a157d645a1f45f63fb8ff73917d368d1d
Alex Shaver <alexshaver@gmail.com>
2020-09-23 12:16:07 -0400

    Initial Commit

---
+++ client/src/App.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+it('renders without crashing', () => {
+// test would need a mock of backend to work correctly
+});
